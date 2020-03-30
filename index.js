0438f5b0818de967bb3d1f8027e8a2ab8dc0d6a8
akmaliavitri <akmaliavitri21@gmail.com>
2020-03-30 20:41:37 +0700

    roman-numerals

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function toRoman(num) {
+    let obj = {
+        'M' : 1000,
+        'CM' : 900,
+        'D' : 500,
+        'CD' : 400,
+        'C' : 100,
+        'XC' : 90,
+        'L' : 50,
+        'XL' : 40,
+        'X' : 10,
+        'XI' : 9,
+        'V' : 5,
+        'IV' : 4,
+        'I' : 1
+    }
 
+    let result = ''
+    for(let i in obj){
+        while(obj[i] <= num){
+            result += i
+            num -= obj[i]
+        }
+    }
+    return result
 }
 
 // Drive code
