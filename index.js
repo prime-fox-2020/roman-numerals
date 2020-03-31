f72b588fda63ac46ebf760f726086634cf716702
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-03-31 17:56:27 +0700

    tugas roman numerals

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function toRoman(num) {
-
+    var romawi = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+    var arab = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    var res = "";
+    
+    for (var i = 0; i < arab.length; i++) {
+        if(num >= arab[i]) {
+            res += romawi[i];
+            num -= arab[i];
+            //  console.log(num);
+            return res + toRoman(num);
+        }
+    }
+    return res;
 }
+
 
 // Drive code
 console.log("My totally sweet testing script for new roman\n");
