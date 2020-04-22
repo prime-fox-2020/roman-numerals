412cfa7934bb4e217a976ffbc4ed54bc95cc2818
Adib Faqih <foezst@gmail.com>
2020-04-22 08:51:49 +0700

    solved

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 function toRoman(num) {
+  const roman = [
+    [1, 'I'],
+    [5, 'V'],
+    [10, 'X'],
+    [50, 'L'],
+    [100, 'C'],
+    [500, 'D'],
+    [1000, 'M']
+  ];
+  if (num <= 0) {
+    return '';
+  }
 
+  let rom, min;
+
+  for (let i = 0; i < roman.length; i++){
+    if (num >= roman[i][0]) {
+      rom = roman[i][1];
+      min = roman[i][0];
+    }
+    if (i % 2 == 0 && roman[i+1] != undefined && num + roman[i][0] >= roman[i+1][0]) {
+      rom = roman[i][1] + roman[i+1][1];
+      min = roman[i+1][0] - roman[i][0];
+    }
+    if (i % 2 != 0 && roman[i+1] != undefined && num + roman[i-1][0] >= roman[i+1][0]) {
+      rom = roman[i-1][1] + roman[i+1][1];
+      min = roman[i+1][0] - roman[i-1][0];
+    }
+  }
+
+  return rom + toRoman(num-min);
 }
 
 // Drive code
